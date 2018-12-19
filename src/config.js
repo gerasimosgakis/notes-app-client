d53e6376de71815a51c0438f0329e559446b8da9
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-12-19 17:33:50 +0000

    feat: configure aws amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "gerasimos-notes-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://jy0m3zwhid.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_JlrTegOzT",
+    APP_CLIENT_ID: "78q83b3rjhkfe8t9m74ibrsnq2",
+    IDENTITY_POOL_ID: "us-east-2:cad5f440-1dc2-41b9-aa6c-b092df44633f"
+  }
+};
